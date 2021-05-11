eafd976d2bc3430108da839ac8d8ad963cab43c5
Kevin Ng <kevin.the.engi@gmail.com>
2021-05-10 17:37:06 -0700

    Complete bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,21 +1,32 @@
-bubbleSort = (array) => {
-  let swapped = false 
-  do {
-    swapped = false
-    array.forEach((current, i) => {
-      console.log(array.join(' '))
-      if (current > array[i + 1]) {
-        const temp = current
-        console.log(array.join(' '))
-        
-        array[i] = array[i + 1]
-        array[i + 1] = temp
-        swapped = true
+let bubbleSort = (array) => {
+  const innerFunction = (arr, isSorted) => {
+    if (isSorted) {
+      return arr;
+    }
+
+    isSorted = true;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      const current = arr[i];
+      const next = arr[i + 1];
+
+      if (current > next) {
+        arr = swapNum(arr, i, i + 1);
+        isSorted = false;
       }
-    })
-  } while(swapped)
-  console.log(array.join(' '))
-  return array
+    }
+    return innerFunction(arr, isSorted);
+  }
+
+  return innerFunction(array, false);
+}
+
+let swapNum = (array, current, next) => {
+  let temp = array[current];
+  array[current] = array[next];
+  array[next] = temp;
+
+  return array;
 }
 
 const numbers = [8, 5, 6, 9, 3, 1, 4, 2, 7, 10]
