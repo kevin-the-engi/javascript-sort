c1a5af6daffd03033d65e2c17f05c30ddb674682
Kevin Ng <kevin.the.engi@gmail.com>
2021-05-11 20:11:47 -0700

    Add insertion sort

--- insertionSort.js
+++ insertionSort.js
@@ -1,15 +1,15 @@
 insertionSort = (array) => {
-  for (outer = 1; outer < array.length; outer++) {
-    for (inner = 0; inner < outer; inner++) {
-      console.log(array.join(' '))
-      if (array[outer] < array[inner]) {
-        const [element] = array.splice(outer, 1)
-        array.splice(inner, 0, element)
-      }
+  for (let i = 1; i < array.length; i++) {
+    let value = array[i];
+    for (var j = i - 1; j >= 0 && array[j] > value; j--) {
+      array[j + 1] = array[j];
     }
+
+    array[j + 1] = value;
   }
-  console.log(array.join(' '))
-  return array
+
+  return array;
 }
+
 const numbers = [8, 5, 6, 9, 3, 1, 4, 2, 7, 10]
 insertionSort(numbers)
